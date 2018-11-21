2101459b6227b741f6053bd843ba963b79042bd6
jepson <jepsonpp@qq.com>
2018-11-21 11:05:23 +0800

    公共的功能完成(侧边栏切换, 二级菜单切换, 退出功能)

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -2,8 +2,6 @@
  * Created by 54721 on 2018/11/21.
  */
 $(function() {
-
-
   //// 开启进度条
   //NProgress.start();
   //
@@ -36,6 +34,50 @@ $(function() {
       // 关闭进度条
       NProgress.done();
     }, 500)
+  });
+
+
+
+
+  // 公用的功能:
+  // 1. 左侧二级切换功能
+  $('#category').click(function() {
+    // 找下一个兄弟元素
+    $(this).next().stop().slideToggle();
   })
+
+
+  // 2. 左侧菜单切换功能
+  $('.lt_topbar .icon_left').click(function() {
+    // 让左侧侧边菜单切换  toggleClass 切换类
+    $('.lt_aside').toggleClass("hidemenu");
+    $('.lt_topbar').toggleClass("hidemenu");
+    $('.lt_main').toggleClass("hidemenu");
+  })
+
+
+  // 3. 公共退出功能
+  $('.lt_topbar .icon_right').click(function() {
+    // 显示退出模态框
+    $('#logoutModal').modal("show");
+  });
+
+  $('#logoutBtn').click(function() {
+
+    // 调用接口, 让后台销毁当前用户的登录状态
+    $.ajax({
+      type: "get",
+      url: "/employee/employeeLogout",
+      dataType: "json",
+      success: function( info ) {
+        if ( info.success ) {
+          // 销毁登录状态成功, 退出成功, 跳转登录页
+          location.href = "login.html";
+        }
+      }
+    })
+
+  })
+
 
 })
